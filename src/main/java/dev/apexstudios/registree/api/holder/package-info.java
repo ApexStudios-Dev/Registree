9994f3325f94e06ffab510eb8a59e4b0521fb3a6
Apex <robsonlawson3@gmail.com>
2025-11-20 14:40:42 +0000

    Swap from JetBrains-Annotations to JSpecify

---
+++ src/main/java/dev/apexstudios/registree/api/holder/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package dev.apexstudios.registree.api.holder;
+
+import org.jspecify.annotations.NullMarked;
--- src/main/java/dev/apexstudios/registree/impl/SimpleRegistree.java
+++ src/main/java/dev/apexstudios/registree/impl/SimpleRegistree.java
@@ -72,7 +72,7 @@ import net.neoforged.neoforge.network.IContainerFactory;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.RegisterEvent;
 import org.apache.commons.lang3.function.Consumers;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 
 public class SimpleRegistree implements Registree {
     protected final String namespace;
