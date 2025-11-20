8b8196b1bac3997c858b4ebb69667c22eba7fde1
Apex <robsonlawson3@gmail.com>
2025-11-20 12:08:23 +0000

    Fix build script using incorrect neoforge version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,12 @@ plugins {
 
 group = "dev.apexstudios"
 
-apex.neoVersion("21.10.41-beta", "2025.10.12")
+apex.neoVersion("21.11.0-alpha.25w45a.20251119.234730", "1.21.10", "2025.10.12")
 apex.extendCompilerErrors()
+
+repositories {
+    apex.neoPrMaven(this, 2815)
+}
 
 tasks.withType<Jar> {
     manifest {
