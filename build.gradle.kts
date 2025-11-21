6dd0779460b3d4c70f9ff8ecfbaeab5eec673800
Apex <robsonlawson3@gmail.com>
2025-11-21 18:13:53 +0000

    Bump NeoForge version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 group = "dev.apexstudios"
 
-apex.neoVersion("21.11.0-alpha.1.21.11-pre1.20251120.181430", "1.21.10", "2025.10.12")
+apex.neoVersion("21.11.0-alpha.1.21.11-pre2.20251121.175959", "1.21.10", "2025.10.12")
 apex.extendCompilerErrors()
 
 repositories {
