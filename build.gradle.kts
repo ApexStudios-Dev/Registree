55328e9049e3b029d3b7d93f2201aba2edbdd6e7
Apex <robsonlawson3@gmail.com>
2025-12-11 15:42:02 +0000

    Bump ApexGradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    id("apex-conventions.neoforge") version "0.1.82"
-    id("apex-conventions.maven-publishing") version "0.1.82"
+    id("apex-conventions.neoforge") version "0.1.83"
+    id("apex-conventions.maven-publishing") version "0.1.83"
+    id("apex-conventions.maven-immaculate") version "0.1.83"
 }
 
 group = "dev.apexstudios"
