6e981cfbf50b0497b9e39b11c2f63e2755aad2d3
Apex <robsonlawson3@gmail.com>
2025-12-20 08:33:57 +0000

    Revert "Refactor build script to not use ApexGradle"
    This reverts commit a6ac96c572e375978d61edbf9f8e7dc1ac53b91a.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,70 +1,23 @@
-import java.text.SimpleDateFormat
-import java.util.*
-
 plugins {
-    `java-library`
-    id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.9"
-    id("net.neoforged.moddev") version "2.0.131"
+    id("apex-conventions.neoforge") version "0.1.90-beta-pr-12"
+    id("apex-conventions.maven-publishing") version "0.1.90-beta-pr-12"
 }
-
-val IS_CI = providers.environmentVariable("CI").map(String::toBoolean).getOrElse(false)
 
 group = "dev.apexstudios"
-version = providers.environmentVariable("VERSION").getOrElse("9.9.999")
-base.archivesName = project.name.lowercase()
 
-idea.module {
-    if(!IS_CI) {
-        isDownloadSources = true
-        isDownloadJavadoc = true
-    }
-
-    excludeDirs.addAll(files(
-        ".gradle",
-        ".idea",
-        "gradle",
-    ))
-}
-
-java {
-    withSourcesJar()
-}
-
-neoForge {
-    version = "26.1.0-alpha.26.1-snapshot-1.20251219.121649"
-}
-
-tasks.withType(Jar::class.java) {
-    manifest {
-        attributes.putAll(mutableMapOf(
-            "Specification-Title" to project.name,
-            "Specification-Vendor" to "ApexStudios",
-            "Specification-Version" to "1",
-
-            "Implementation-Title" to project.name,
-            "Implementation-Vendor" to "ApexStudios",
-            "Implementation-Version" to project.version,
-            "Implementation-Timestamp" to SimpleDateFormat("dd-MM-yyyy'T'HH:mm:ssZ").format(Date()),
-
-            "FMLModType" to "GAMELIBRARY",
-            "Automatic-Module-Name" to "registree"
-        ))
-    }
-}
-
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-}
+apex.neoVersion("26.1.0-alpha.26.1-snapshot-1.20251219.121649")
+apex.extendCompilerErrors()
 
 repositories {
-    maven("https://maven.apexstudios.dev/proxy")
+    apex.neoPrMaven(this, 2879)
+    maven("https://maven.apexstudios.dev/prs/ApexGradle/pr12")
+}
 
-    maven("https://prmaven.neoforged.net/NeoForge/pr2879") {
-        name = "NeoForge - PR #2879"
-
-        content {
-            includeModule("net.neoforged", "testframework")
-            includeModule("net.neoforged", "neoforge")
-        }
+tasks.withType<Jar> {
+    manifest {
+        attributes(
+            "FMLModType" to "GAMELIBRARY",
+            "Automatic-Module-Name" to "registree"
+        )
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 pluginManagement {
     repositories {
         maven("https://maven.apexstudios.dev/proxy")
+        maven("https://maven.apexstudios.dev/prs/ApexGradle/pr12")
         gradlePluginPortal()
     }
 }
