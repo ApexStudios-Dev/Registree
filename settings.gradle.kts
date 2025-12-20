fb96b54af4794f911f2bd06756de6b469894b14c
Apex <robsonlawson3@gmail.com>
2025-12-20 07:55:33 +0000

    Bump ApexGradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
 pluginManagement {
     repositories {
         maven("https://maven.apexstudios.dev/proxy")
-        maven("https://prmaven.neoforged.net/ModDevGradle/pr298")
         maven("https://maven.apexstudios.dev/prs/ApexGradle/pr12")
         gradlePluginPortal()
     }
