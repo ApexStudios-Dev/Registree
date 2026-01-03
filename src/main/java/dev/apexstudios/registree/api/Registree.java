9a64c51991357bf3f4806f1ea61309e32c89c7e0
Apex <robsonlawson3@gmail.com>
2026-01-03 07:13:25 +0000

    Apply formatting

--- src/main/java/dev/apexstudios/registree/api/Registree.java
+++ src/main/java/dev/apexstudios/registree/api/Registree.java
@@ -83,7 +83,7 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import org.apache.commons.lang3.ArrayUtils;
 import org.jetbrains.annotations.ApiStatus;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 
 /// Base interface for all Registree types
 ///
