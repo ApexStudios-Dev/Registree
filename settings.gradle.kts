bf561e6f622680ce074e17e36dc1a71cbf592514
Apex <robsonlawson3@gmail.com>
2026-01-11 22:22:11 +0000

    Bump ApexGradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if(requested.id.namespace == "apex-conventions") {
-                useVersion("0.1.90")
+                useVersion("0.1.91")
             }
         }
     }
