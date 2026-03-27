9d3220e287a47438d1bbc5731753d80a14ea0dd9
Apex <robsonlawson3@gmail.com>
2026-03-27 22:57:43 +0000

    Rework build scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,19 @@
 plugins {
-    id("apex-conventions.neoforge")
-    id("apex-conventions.maven-publishing")
+    `java-library`
+    `maven-publish`
+
+    id("net.neoforged.moddev") version "2.0.141"
     id("apex-conventions.jspecify")
 }
 
 group = "dev.apexstudios"
+base.archivesName = "registree"
+version = providers.environmentVariable("VERSION").getOrElse("0.0NONE")
 neoForge.version = libs.versions.neoforge.get()
 
-repositories {
+java {
+    toolchain.vendor.set(JvmVendorSpec.JETBRAINS)
+    withSourcesJar()
 }
 
 tasks.withType<Jar> {
@@ -16,5 +22,32 @@ tasks.withType<Jar> {
             "FMLModType" to "GAMELIBRARY",
             "Automatic-Module-Name" to "registree"
         )
+    }
+}
+
+publishing {
+    publications.create("release", MavenPublication::class.java) {
+        afterEvaluate {
+            groupId = "dev.apexstudios"
+            artifactId = "registree"
+            version = project.version as String
+        }
+
+        from(components["java"])
+    }
+
+    repositories {
+        if(System.getenv("MAVEN_USERNAME") != null && System.getenv("MAVEN_PASSWORD") != null) {
+            maven("https://maven.apexmodder.com/releases") {
+                name = "ApexStudios-Releases"
+
+                credentials {
+                    username = System.getenv("MAVEN_USERNAME")
+                    password = System.getenv("MAVEN_PASSWORD")
+                }
+
+                authentication.create<BasicAuthentication>("basic")
+            }
+        }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,10 +4,14 @@ pluginManagement {
         gradlePluginPortal()
     }
 
-    resolutionStrategy {
-        eachPlugin {
-            if(requested.id.namespace == "apex-conventions") {
-                useVersion("0.1.94")
+    if(file("../../ApexGradle").exists()) {
+        includeBuild("../../ApexGradle")
+    } else {
+        resolutionStrategy {
+            eachPlugin {
+                if(requested.id.namespace == "apex-conventions") {
+                    useVersion("0.1.94")
+                }
             }
         }
     }
@@ -15,7 +19,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     versionCatalogs.create("libs") {
-        version("neoforge", "26.1.0.1-beta")
+        version("neoforge", "26.1.0.7-beta")
     }
 }
 
