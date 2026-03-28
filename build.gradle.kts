8a1a3d30d2cf97c031d745fffd51ce04e08012b6
Apex <robsonlawson3@gmail.com>
2026-03-28 03:18:55 +0000

    Add tasks to root project to ensure they run for sub projects

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,18 @@ plugins {
     id("net.fabricmc.fabric-loom-companion") version "1.14.4" apply false
     id("net.fabricmc.fabric-loom") version "1.14-SNAPSHOT" apply false
 }
+
+tasks.register("applyAllFormatting") {
+    group = "verification"
+    dependsOn(subprojects.map { it.tasks.findByName("applyAllFormatting") })
+}
+
+tasks.register("checkFormatting") {
+    group = "verification"
+    dependsOn(subprojects.map { it.tasks.findByName("checkFormatting") })
+}
+
+tasks.register("generatePackageInfos") {
+    group = "verification"
+    dependsOn(subprojects.map { it.tasks.findByName("generatePackageInfos") })
+}
