b99f2222260b70787b44fa1cb32a0e0de5e3d2e8
Apex <robsonlawson3@gmail.com>
2026-04-06 09:38:57 +0100

    Revert build script changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,15 @@
 plugins {
-    `java-library`
-    `maven-publish`
-
-    id("net.neoforged.moddev") version "2.0.141"
+    id("apex-conventions.neoforge")
+    id("apex-conventions.maven-publishing")
     id("apex-conventions.jspecify")
 }
 
 group = "dev.apexstudios"
-base.archivesName = "registree"
-version = providers.environmentVariable("VERSION").getOrElse("0.0NONE")
 neoForge.version = libs.versions.neoforge.get()
 
-java {
-    toolchain.vendor.set(JvmVendorSpec.JETBRAINS)
-    withSourcesJar()
-}
-
-tasks.withType<Jar> {
+tasks.withType(Jar::class.java) {
     manifest {
-        attributes(
-            "FMLModType" to "GAMELIBRARY",
-            "Automatic-Module-Name" to "registree"
-        )
-    }
-}
-
-publishing {
-    publications.create("release", MavenPublication::class.java) {
-        afterEvaluate {
-            groupId = "dev.apexstudios"
-            artifactId = "registree"
-            version = project.version as String
-        }
-
-        from(components["java"])
-    }
-
-    repositories {
-        if(System.getenv("MAVEN_USERNAME") != null && System.getenv("MAVEN_PASSWORD") != null) {
-            maven("https://maven.apexmodder.com/releases") {
-                name = "ApexStudios-Releases"
-
-                credentials {
-                    username = System.getenv("MAVEN_USERNAME")
-                    password = System.getenv("MAVEN_PASSWORD")
-                }
-
-                authentication.create<BasicAuthentication>("basic")
-            }
-        }
+        attributes["FMLModType"] = neoForge.minecraftVersion
+        attributes["Automatic-Module-Name"] = project.name.lowercase()
     }
 }
